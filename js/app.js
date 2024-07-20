3bc7dc2d8495607b62659d28f3a4e14ed1bf9edb
Yashique <yashiquechalil@gmail.com>
2024-07-20 16:08:53 +0100

    rnbo config

--- js/app.js
+++ js/app.js
@@ -1,6 +1,14 @@
 async function setup() {
     const patchExportURL = "export/patch.export.json";
 
+    canvas = createCanvas(720, 720);
+    
+    colorMode(HSB, 360, 100, 100);
+
+    rectMode(CENTER);
+
+    noStroke();
+
     // Create AudioContext
     const WAContext = window.AudioContext || window.webkitAudioContext;
     const context = new WAContext();
@@ -65,32 +73,34 @@ async function setup() {
     }
 
     // (Optional) Load the samples
-    if (dependencies.length)
-        await device.loadDataBufferDependencies(dependencies);
+    //if (dependencies.length)
+    //    await device.loadDataBufferDependencies(dependencies);
 
     // Connect the device to the web audio graph
     device.node.connect(outputNode);
 
     // (Optional) Extract the name and rnbo version of the patcher from the description
-    document.getElementById("patcher-title").innerText = (patcher.desc.meta.filename || "Unnamed Patcher") + " (v" + patcher.desc.meta.rnboversion + ")";
+    //document.getElementById("patcher-title").innerText = (patcher.desc.meta.filename || "Unnamed Patcher") + " (v" + patcher.desc.meta.rnboversion + ")";
 
     // (Optional) Automatically create sliders for the device parameters
-    makeSliders(device);
+    //makeSliders(device);
 
     // (Optional) Create a form to send messages to RNBO inputs
-    makeInportForm(device);
+    //makeInportForm(device);
 
     // (Optional) Attach listeners to outports so you can log messages from the RNBO patcher
-    attachOutports(device);
+    //attachOutports(device);
 
     // (Optional) Load presets, if any
-    loadPresets(device, patcher);
+    //loadPresets(device, patcher);
 
     // (Optional) Connect MIDI inputs
-    makeMIDIKeyboard(device);
+   // makeMIDIKeyboard(device);
 
     document.body.onclick = () => {
         context.resume();
+        startParam.enumValue = 1;
+
     }
 
     // Skip if you're not using guardrails.js
@@ -112,101 +122,105 @@ function loadRNBOScript(version) {
         };
         document.body.append(el);
     });
+
+    startParam = device.parametersById.get('start');
+    mixParam = device.parametersById.get('doomFuzz/Mix');
+    buzzParam = device.parametersById.get('doomFuzz/DoomFuzzDSP/Fuzz/Buzz');
 }
 
-function makeSliders(device) {
-    let pdiv = document.getElementById("rnbo-parameter-sliders");
-    let noParamLabel = document.getElementById("no-param-label");
-    if (noParamLabel && device.numParameters > 0) pdiv.removeChild(noParamLabel);
+// function makeSliders(device) {
+//     let pdiv = document.getElementById("rnbo-parameter-sliders");
+//     let noParamLabel = document.getElementById("no-param-label");
+//     if (noParamLabel && device.numParameters > 0) pdiv.removeChild(noParamLabel);
 
-    // This will allow us to ignore parameter update events while dragging the slider.
-    let isDraggingSlider = false;
-    let uiElements = {};
+//     // This will allow us to ignore parameter update events while dragging the slider.
+//     let isDraggingSlider = false;
+//     let uiElements = {};
 
-    device.parameters.forEach(param => {
-        // Subpatchers also have params. If we want to expose top-level
-        // params only, the best way to determine if a parameter is top level
-        // or not is to exclude parameters with a '/' in them.
-        // You can uncomment the following line if you don't want to include subpatcher params
+//     device.parameters.forEach(param => {
+//         // Subpatchers also have params. If we want to expose top-level
+//         // params only, the best way to determine if a parameter is top level
+//         // or not is to exclude parameters with a '/' in them.
+//         // You can uncomment the following line if you don't want to include subpatcher params
         
-        //if (param.id.includes("/")) return;
+//         //if (param.id.includes("/")) return;
 
-        // Create a label, an input slider and a value display
-        let label = document.createElement("label");
-        let slider = document.createElement("input");
-        let text = document.createElement("input");
-        let sliderContainer = document.createElement("div");
-        sliderContainer.appendChild(label);
-        sliderContainer.appendChild(slider);
-        sliderContainer.appendChild(text);
+//         // Create a label, an input slider and a value display
+//         let label = document.createElement("label");
+//         let slider = document.createElement("input");
+//         let text = document.createElement("input");
+//         let sliderContainer = document.createElement("div");
+//         sliderContainer.appendChild(label);
+//         sliderContainer.appendChild(slider);
+//         sliderContainer.appendChild(text);
 
-        // Add a name for the label
-        label.setAttribute("name", param.name);
-        label.setAttribute("for", param.name);
-        label.setAttribute("class", "param-label");
-        label.textContent = `${param.name}: `;
+//         // Add a name for the label
+//         label.setAttribute("name", param.name);
+//         label.setAttribute("for", param.name);
+//         label.setAttribute("class", "param-label");
+//         label.textContent = `${param.name}: `;
 
-        // Make each slider reflect its parameter
-        slider.setAttribute("type", "range");
-        slider.setAttribute("class", "param-slider");
-        slider.setAttribute("id", param.id);
-        slider.setAttribute("name", param.name);
-        slider.setAttribute("min", param.min);
-        slider.setAttribute("max", param.max);
-        if (param.steps > 1) {
-            slider.setAttribute("step", (param.max - param.min) / (param.steps - 1));
-        } else {
-            slider.setAttribute("step", (param.max - param.min) / 1000.0);
-        }
-        slider.setAttribute("value", param.value);
+//         // Make each slider reflect its parameter
+//         slider.setAttribute("type", "range");
+//         slider.setAttribute("class", "param-slider");
+//         slider.setAttribute("id", param.id);
+//         slider.setAttribute("name", param.name);
+//         slider.setAttribute("min", param.min);
+//         slider.setAttribute("max", param.max);
+//         if (param.steps > 1) {
+//             slider.setAttribute("step", (param.max - param.min) / (param.steps - 1));
+//         } else {
+//             slider.setAttribute("step", (param.max - param.min) / 1000.0);
+//         }
+//         slider.setAttribute("value", param.value);
 
-        // Make a settable text input display for the value
-        text.setAttribute("value", param.value.toFixed(1));
-        text.setAttribute("type", "text");
+//         // Make a settable text input display for the value
+//         text.setAttribute("value", param.value.toFixed(1));
+//         text.setAttribute("type", "text");
 
-        // Make each slider control its parameter
-        slider.addEventListener("pointerdown", () => {
-            isDraggingSlider = true;
-        });
-        slider.addEventListener("pointerup", () => {
-            isDraggingSlider = false;
-            slider.value = param.value;
-            text.value = param.value.toFixed(1);
-        });
-        slider.addEventListener("input", () => {
-            let value = Number.parseFloat(slider.value);
-            param.value = value;
-        });
+//         // Make each slider control its parameter
+//         slider.addEventListener("pointerdown", () => {
+//             isDraggingSlider = true;
+//         });
+//         slider.addEventListener("pointerup", () => {
+//             isDraggingSlider = false;
+//             slider.value = param.value;
+//             text.value = param.value.toFixed(1);
+//         });
+//         slider.addEventListener("input", () => {
+//             let value = Number.parseFloat(slider.value);
+//             param.value = value;
+//         });
 
-        // Make the text box input control the parameter value as well
-        text.addEventListener("keydown", (ev) => {
-            if (ev.key === "Enter") {
-                let newValue = Number.parseFloat(text.value);
-                if (isNaN(newValue)) {
-                    text.value = param.value;
-                } else {
-                    newValue = Math.min(newValue, param.max);
-                    newValue = Math.max(newValue, param.min);
-                    text.value = newValue;
-                    param.value = newValue;
-                }
-            }
-        });
+//         // Make the text box input control the parameter value as well
+//         text.addEventListener("keydown", (ev) => {
+//             if (ev.key === "Enter") {
+//                 let newValue = Number.parseFloat(text.value);
+//                 if (isNaN(newValue)) {
+//                     text.value = param.value;
+//                 } else {
+//                     newValue = Math.min(newValue, param.max);
+//                     newValue = Math.max(newValue, param.min);
+//                     text.value = newValue;
+//                     param.value = newValue;
+//                 }
+//             }
+//         });
 
-        // Store the slider and text by name so we can access them later
-        uiElements[param.id] = { slider, text };
+//         // Store the slider and text by name so we can access them later
+//         uiElements[param.id] = { slider, text };
 
-        // Add the slider element
-        pdiv.appendChild(sliderContainer);
-    });
+//         // Add the slider element
+//         pdiv.appendChild(sliderContainer);
+//     });
 
-    // Listen to parameter changes from the device
-    device.parameterChangeEvent.subscribe(param => {
-        if (!isDraggingSlider)
-            uiElements[param.id].slider.value = param.value;
-        uiElements[param.id].text.value = param.value.toFixed(1);
-    });
-}
+//     // Listen to parameter changes from the device
+//     device.parameterChangeEvent.subscribe(param => {
+//         if (!isDraggingSlider)
+//             uiElements[param.id].slider.value = param.value;
+//         uiElements[param.id].text.value = param.value.toFixed(1);
+//     });
+// }
 
 function makeInportForm(device) {
     const idiv = document.getElementById("rnbo-inports");
@@ -267,73 +281,73 @@ function attachOutports(device) {
     });
 }
 
-function loadPresets(device, patcher) {
-    let presets = patcher.presets || [];
-    if (presets.length < 1) {
-        document.getElementById("rnbo-presets").removeChild(document.getElementById("preset-select"));
-        return;
-    }
+// function loadPresets(device, patcher) {
+//     let presets = patcher.presets || [];
+//     if (presets.length < 1) {
+//         document.getElementById("rnbo-presets").removeChild(document.getElementById("preset-select"));
+//         return;
+//     }
 
-    document.getElementById("rnbo-presets").removeChild(document.getElementById("no-presets-label"));
-    let presetSelect = document.getElementById("preset-select");
-    presets.forEach((preset, index) => {
-        const option = document.createElement("option");
-        option.innerText = preset.name;
-        option.value = index;
-        presetSelect.appendChild(option);
-    });
-    presetSelect.onchange = () => device.setPreset(presets[presetSelect.value].preset);
-}
+//     document.getElementById("rnbo-presets").removeChild(document.getElementById("no-presets-label"));
+//     let presetSelect = document.getElementById("preset-select");
+//     presets.forEach((preset, index) => {
+//         const option = document.createElement("option");
+//         option.innerText = preset.name;
+//         option.value = index;
+//         presetSelect.appendChild(option);
+//     });
+//     presetSelect.onchange = () => device.setPreset(presets[presetSelect.value].preset);
+// }
 
-function makeMIDIKeyboard(device) {
-    let mdiv = document.getElementById("rnbo-clickable-keyboard");
-    if (device.numMIDIInputPorts === 0) return;
+// function makeMIDIKeyboard(device) {
+//     let mdiv = document.getElementById("rnbo-clickable-keyboard");
+//     if (device.numMIDIInputPorts === 0) return;
 
-    mdiv.removeChild(document.getElementById("no-midi-label"));
+//     mdiv.removeChild(document.getElementById("no-midi-label"));
 
-    const midiNotes = [49, 52, 56, 63];
-    midiNotes.forEach(note => {
-        const key = document.createElement("div");
-        const label = document.createElement("p");
-        label.textContent = note;
-        key.appendChild(label);
-        key.addEventListener("pointerdown", () => {
-            let midiChannel = 0;
+//     const midiNotes = [49, 52, 56, 63];
+//     midiNotes.forEach(note => {
+//         const key = document.createElement("div");
+//         const label = document.createElement("p");
+//         label.textContent = note;
+//         key.appendChild(label);
+//         key.addEventListener("pointerdown", () => {
+//             let midiChannel = 0;
 
-            // Format a MIDI message paylaod, this constructs a MIDI on event
-            let noteOnMessage = [
-                144 + midiChannel, // Code for a note on: 10010000 & midi channel (0-15)
-                note, // MIDI Note
-                100 // MIDI Velocity
-            ];
+//             // Format a MIDI message paylaod, this constructs a MIDI on event
+//             let noteOnMessage = [
+//                 144 + midiChannel, // Code for a note on: 10010000 & midi channel (0-15)
+//                 note, // MIDI Note
+//                 100 // MIDI Velocity
+//             ];
         
-            let noteOffMessage = [
-                128 + midiChannel, // Code for a note off: 10000000 & midi channel (0-15)
-                note, // MIDI Note
-                0 // MIDI Velocity
-            ];
+//             let noteOffMessage = [
+//                 128 + midiChannel, // Code for a note off: 10000000 & midi channel (0-15)
+//                 note, // MIDI Note
+//                 0 // MIDI Velocity
+//             ];
         
-            // Including rnbo.min.js (or the unminified rnbo.js) will add the RNBO object
-            // to the global namespace. This includes the TimeNow constant as well as
-            // the MIDIEvent constructor.
-            let midiPort = 0;
-            let noteDurationMs = 250;
+//             // Including rnbo.min.js (or the unminified rnbo.js) will add the RNBO object
+//             // to the global namespace. This includes the TimeNow constant as well as
+//             // the MIDIEvent constructor.
+//             let midiPort = 0;
+//             let noteDurationMs = 250;
         
-            // When scheduling an event to occur in the future, use the current audio context time
-            // multiplied by 1000 (converting seconds to milliseconds) for now.
-            let noteOnEvent = new RNBO.MIDIEvent(device.context.currentTime * 1000, midiPort, noteOnMessage);
-            let noteOffEvent = new RNBO.MIDIEvent(device.context.currentTime * 1000 + noteDurationMs, midiPort, noteOffMessage);
+//             // When scheduling an event to occur in the future, use the current audio context time
+//             // multiplied by 1000 (converting seconds to milliseconds) for now.
+//             let noteOnEvent = new RNBO.MIDIEvent(device.context.currentTime * 1000, midiPort, noteOnMessage);
+//             let noteOffEvent = new RNBO.MIDIEvent(device.context.currentTime * 1000 + noteDurationMs, midiPort, noteOffMessage);
         
-            device.scheduleEvent(noteOnEvent);
-            device.scheduleEvent(noteOffEvent);
+//             device.scheduleEvent(noteOnEvent);
+//             device.scheduleEvent(noteOffEvent);
 
-            key.classList.add("clicked");
-        });
+//             key.classList.add("clicked");
+//         });
 
-        key.addEventListener("pointerup", () => key.classList.remove("clicked"));
+//         key.addEventListener("pointerup", () => key.classList.remove("clicked"));
 
-        mdiv.appendChild(key);
-    });
-}
+//         mdiv.appendChild(key);
+//     });
+// }
 
 setup();
