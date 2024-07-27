09a0d05653702698c531a3b4941dca2a9308a0a9
Yashique <yashiquechalil@gmail.com>
2024-07-27 13:11:13 +0100

    using p5 context

--- js/app.js
+++ js/app.js
@@ -1,414 +1,142 @@
-let buzzParam;
-let startParam;
-let mixParam;
-let isPlaying = false;
 
+let canvas, w, h, sketchStarted = false, context, synth
 
+// setup RNBO and connect to p5 context
+async function rnboSetup(context) { // pass in context from p5
+  const outputNode = context.createGain()
+  outputNode.connect(context.destination)
 
-async function setup() {
-    const patchExportURL = "export/patch.export.json";
+  // load reverb patch
+  //response = await fetch("export/rnbo.shimmerev.json")
+  //const reverbPatcher = await response.json()
 
-    canvas = createCanvas(800, 800);
-    canvas.position((windowWidth -800) /2, (windowHeight - 800)/2);
-    
-    colorMode(HSB, 360, 100, 100);
+      // Fetch the exported patcher
+      let response, patcher;
+      try {
+          response = await fetch(patchExportURL);
+          doomPatcher = await response.json();
+      
+          if (!window.RNBO) {
+              // Load RNBO script dynamically
+              // Note that you can skip this by knowing the RNBO version of your patch
+              // beforehand and just include it using a <script> tag
+              await loadRNBOScript(patcher.desc.meta.rnboversion);
+          }
+  
+      } catch (err) {
+          const errorContext = {
+              error: err
+          };
+          if (response && (response.status >= 300 || response.status < 200)) {
+              errorContext.header = `Couldn't load patcher export bundle`,
+              errorContext.description = `Check app.js to see what file it's trying to load. Currently it's` +
+              ` trying to load "${patchExportURL}". If that doesn't` + 
+              ` match the name of the file you exported from RNBO, modify` + 
+              ` patchExportURL in app.js.`;
+          }
+          if (typeof guardrails === "function") {
+              guardrails(errorContext);
+          } else {
+              throw err;
+          }
+          return;
+      }
 
+  const doomDevice = await RNBO.createDevice({ context, patcher: doomPatcher })
 
+  // establish signal chain: p5 Synth → Reverb Patch → Output
+  // connect synth to reverb patch
+ // synth.connect(doomDevice.node)
 
-    rectMode(CENTER);
-
-    noStroke();
-
-    // Create AudioContext
-    const WAContext = window.AudioContext || window.webkitAudioContext;
-    const context = new WAContext();
-
-
-    // Create gain node and connect it to audio output
-    const outputNode = context.createGain();
-    outputNode.connect(context.destination);
-
-    
-    // Fetch the exported patcher
-    let response, patcher;
-    try {
-        response = await fetch(patchExportURL);
-        patcher = await response.json();
-    
-        if (!window.RNBO) {
-            // Load RNBO script dynamically
-            // Note that you can skip this by knowing the RNBO version of your patch
-            // beforehand and just include it using a <script> tag
-            await loadRNBOScript(patcher.desc.meta.rnboversion);
-        }
-
-    } catch (err) {
-        const errorContext = {
-            error: err
-        };
-        if (response && (response.status >= 300 || response.status < 200)) {
-            errorContext.header = `Couldn't load patcher export bundle`,
-            errorContext.description = `Check app.js to see what file it's trying to load. Currently it's` +
-            ` trying to load "${patchExportURL}". If that doesn't` + 
-            ` match the name of the file you exported from RNBO, modify` + 
-            ` patchExportURL in app.js.`;
-        }
-        if (typeof guardrails === "function") {
-            guardrails(errorContext);
-        } else {
-            throw err;
-        }
-        return;
-    }
-    
-    // (Optional) Fetch the dependencies
-    let dependencies = [];
-    try {
-        const dependenciesResponse = await fetch("export/dependencies.json");
-        dependencies = await dependenciesResponse.json();
-
-        // Prepend "export" to any file dependenciies
-        dependencies = dependencies.map(d => d.file ? Object.assign({}, d, { file: "export/" + d.file }) : d);
-    } catch (e) {}
-
-    // Create the device
-    let device;
-    try {
-        device = await RNBO.createDevice({ context, patcher });
-    } catch (err) {
-        if (typeof guardrails === "function") {
-            guardrails({ error: err });
-        } else {
-            throw err;
-        }
-        return;
-    }
-
-
-    // (Optional) Load the samples
-    //if (dependencies.length)
-    //    await device.loadDataBufferDependencies(dependencies);
-
-    // Connect the device to the web audio graph
-    device.node.connect(outputNode);
-
-    // (Optional) Extract the name and rnbo version of the patcher from the description
-    //document.getElementById("patcher-title").innerText = (patcher.desc.meta.filename || "Unnamed Patcher") + " (v" + patcher.desc.meta.rnboversion + ")";
-
-    // (Optional) Automatically create sliders for the device parameters
-    //makeSliders(device);
-
-    // (Optional) Create a form to send messages to RNBO inputs
-    //makeInportForm(device);
-
-    // (Optional) Attach listeners to outports so you can log messages from the RNBO patcher
-    //attachOutports(device);
-
-    // (Optional) Load presets, if any
-    //loadPresets(device, patcher);
-
-    // (Optional) Connect MIDI inputs
-   // makeMIDIKeyboard(device);
-
-   startParam = device.parametersById.get('start');
-   mixParam = device.parametersById.get('doomFuzz/Mix');
-   buzzParam = device.parametersById.get('doomFuzz/DoomFuzzDSP/Fuzz/Buzz');
-
-    document.body.onclick = () => {
-
-        if (context.state === 'suspended'){
-            context.resume();
-        }
-        
-        if (isPlaying == false)
-        {
-            startParam.enumValue = 'start';
-            isPlaying = true;
-        }
-        else 
-        {
-            startParam.enumValue = 'stop';
-            isPlaying = false;
-        }
-        
-
-    }
-
-    // Skip if you're not using guardrails.js
-    if (typeof guardrails === "function")
-        guardrails();
+  // connect reverb patch to output
+  doomDevice.node.connect(outputNode)
+  context.suspend()
 }
 
-function loadRNBOScript(version) {
-    return new Promise((resolve, reject) => {
-        if (/^\d+\.\d+\.\d+-dev$/.test(version)) {
-            throw new Error("Patcher exported with a Debug Version!\nPlease specify the correct RNBO version to use in the code.");
-        }
-        const el = document.createElement("script");
-        el.src = "https://c74-public.nyc3.digitaloceanspaces.com/rnbo/" + encodeURIComponent(version) + "/rnbo.min.js";
-        el.onload = resolve;
-        el.onerror = function(err) {
-            console.log(err);
-            reject(new Error("Failed to load rnbo.js v" + version));
-        };
-        document.body.append(el);
-    });
+// this gets called once during initialization
+function setup() {
+  w = window.innerWidth // width of the browser window
+  h = window.innerHeight // height of the browser window
 
+  startParam = device.parametersById.get('start');
+  mixParam = device.parametersById.get('doomFuzz/Mix');
+  buzzParam = device.parametersById.get('doomFuzz/DoomFuzzDSP/Fuzz/Buzz');
 
+  // create a canvas for drawing, with dimensions 500x500px
+  canvas = createCanvas(w, h) 
+
+  // make the background of the canvas yellow
+  background('yellow') 
+
+  // fill any shapes that you draw on screen with red
+  fill('red') 
+
+  // don't add any strokes/outlines to shapes
+  // by default they have a black stroke
+  noStroke() 
+
+  // create button - the text inside the function call
+  // is the text displayed on screen
+  startButton = createButton('Start Sketch') 
+
+  // position the button at the center of the screen
+  startButton.position(w/2, h/2)
+
+  // tell the button what function to call when it is pressed
+  startButton.mousePressed(resumeAudio) 
+
+  context = getAudioContext() // get p5 audio context
+
+ // synth = new p5.MonoSynth() // create a synth
+ // synth.setADSR(10, 1, 1, 5) // set an envelope
+ // synth.amp(0.1) // set a lower amplitude to be careful with volumes
+
+  rnboSetup(context) // call RNBO setup function and pass in context
 }
 
-function makeSliders(device) {
-    let pdiv = document.getElementById("rnbo-parameter-sliders");
-    let noParamLabel = document.getElementById("no-param-label");
-    if (noParamLabel && device.numParameters > 0) pdiv.removeChild(noParamLabel);
 
-    // This will allow us to ignore parameter update events while dragging the slider.
-    let isDraggingSlider = false;
-    let uiElements = {};
+// built-in p5 function that is called when the mouse is pressed
+function mousePressed() {
+  // check that the audio is started
+  if(sketchStarted == true) {
+    // mouseX gets the X-coordinate of the mouse press
+    // and maps the value from the range 0 - 500
+    // to 12 (C0) - 108 (C8)
+    let note = map(mouseX, 0, w, 12, 108)
 
-    device.parameters.forEach(param => {
-        // Subpatchers also have params. If we want to expose top-level
-        // params only, the best way to determine if a parameter is top level
-        // or not is to exclude parameters with a '/' in them.
-        // You can uncomment the following line if you don't want to include subpatcher params
-        
-        //if (param.id.includes("/")) return;
+    // play the note above, with 90 velocity
+    // right now, for 0.1 seconds
+    // the duration gets compounded with the envelope
+    // synth.play(midiToFreq(note), 90, 0, 0.1)
+    startParam.enumValue = 'start';
 
-        // Create a label, an input slider and a value display
-        let label = document.createElement("label");
-        let slider = document.createElement("input");
-        let text = document.createElement("input");
-        let sliderContainer = document.createElement("div");
-        sliderContainer.appendChild(label);
-        sliderContainer.appendChild(slider);
-        sliderContainer.appendChild(text);
-
-        // Add a name for the label
-        label.setAttribute("name", param.name);
-        label.setAttribute("for", param.name);
-        label.setAttribute("class", "param-label");
-        label.textContent = `${param.name}: `;
-
-        // Make each slider reflect its parameter
-        slider.setAttribute("type", "range");
-        slider.setAttribute("class", "param-slider");
-        slider.setAttribute("id", param.id);
-        slider.setAttribute("name", param.name);
-        slider.setAttribute("min", param.min);
-        slider.setAttribute("max", param.max);
-        if (param.steps > 1) {
-            slider.setAttribute("step", (param.max - param.min) / (param.steps - 1));
-        } else {
-            slider.setAttribute("step", (param.max - param.min) / 1000.0);
-        }
-        slider.setAttribute("value", param.value);
-
-        // Make a settable text input display for the value
-        text.setAttribute("value", param.value.toFixed(1));
-        text.setAttribute("type", "text");
-
-        // Make each slider control its parameter
-        slider.addEventListener("pointerdown", () => {
-            isDraggingSlider = true;
-        });
-        slider.addEventListener("pointerup", () => {
-            isDraggingSlider = false;
-            slider.value = param.value;
-            text.value = param.value.toFixed(1);
-        });
-        slider.addEventListener("input", () => {
-            let value = Number.parseFloat(slider.value);
-            param.value = value;
-        });
-
-        // Make the text box input control the parameter value as well
-        text.addEventListener("keydown", (ev) => {
-            if (ev.key === "Enter") {
-                let newValue = Number.parseFloat(text.value);
-                if (isNaN(newValue)) {
-                    text.value = param.value;
-                } else {
-                    newValue = Math.min(newValue, param.max);
-                    newValue = Math.max(newValue, param.min);
-                    text.value = newValue;
-                    param.value = newValue;
-                }
-            }
-        });
-
-        // Store the slider and text by name so we can access them later
-        uiElements[param.id] = { slider, text };
-
-        // Add the slider element
-        pdiv.appendChild(sliderContainer);
-    });
-
-    // Listen to parameter changes from the device
-    device.parameterChangeEvent.subscribe(param => {
-        if (!isDraggingSlider)
-            uiElements[param.id].slider.value = param.value;
-        uiElements[param.id].text.value = param.value.toFixed(1);
-    });
+    // draw an ellipse at the X and Y coordinates
+    // with a random size between 0 and 200px
+    ellipse(mouseX, mouseY, random(200))
+  }
 }
 
-function makeInportForm(device) {
-    const idiv = document.getElementById("rnbo-inports");
-    const inportSelect = document.getElementById("inport-select");
-    const inportText = document.getElementById("inport-text");
-    const inportForm = document.getElementById("inport-form");
-    let inportTag = null;
-    
-    // Device messages correspond to inlets/outlets or inports/outports
-    // You can filter for one or the other using the "type" of the message
-    const messages = device.messages;
-    const inports = messages.filter(message => message.type === RNBO.MessagePortType.Inport);
 
-    if (inports.length === 0) {
-        idiv.removeChild(document.getElementById("inport-form"));
-        return;
-    } else {
-        idiv.removeChild(document.getElementById("no-inports-label"));
-        inports.forEach(inport => {
-            const option = document.createElement("option");
-            option.innerText = inport.tag;
-            inportSelect.appendChild(option);
-        });
-        inportSelect.onchange = () => inportTag = inportSelect.value;
-        inportTag = inportSelect.value;
+// function that will be called when startButton is pressed
+function resumeAudio() {
+  sketchStarted = true // audio is now started
 
-        inportForm.onsubmit = (ev) => {
-            // Do this or else the page will reload
-            ev.preventDefault();
+  // change CSS of button to hide it
+  // since we don't need it anymore
+  startButton.style('opacity', '0') 
 
-            // Turn the text into a list of numbers (RNBO messages must be numbers, not text)
-            const values = inportText.value.split(/\s+/).map(s => parseFloat(s));
-            
-            // Send the message event to the RNBO device
-            let messageEvent = new RNBO.MessageEvent(RNBO.TimeNow, inportTag, values);
-            device.scheduleEvent(messageEvent);
-        }
-    }
+  // get the audio context from p5
+  if (getAudioContext().state !== 'running') {
+    // and resume it if it's not running already 
+    context.resume() 
+  }
 }
 
-function attachOutports(device) {
-    const outports = device.outports;
-    if (outports.length < 1) {
-        document.getElementById("rnbo-console").removeChild(document.getElementById("rnbo-console-div"));
-        return;
-    }
 
-    document.getElementById("rnbo-console").removeChild(document.getElementById("no-outports-label"));
-    device.messageEvent.subscribe((ev) => {
-
-        // Ignore message events that don't belong to an outport
-        if (outports.findIndex(elt => elt.tag === ev.tag) < 0) return;
-
-        // Message events have a tag as well as a payload
-        console.log(`${ev.tag}: ${ev.payload}`);
-
-        document.getElementById("rnbo-console-readout").innerText = `${ev.tag}: ${ev.payload}`;
-    });
-}
-
-function loadPresets(device, patcher) {
-    let presets = patcher.presets || [];
-    if (presets.length < 1) {
-        document.getElementById("rnbo-presets").removeChild(document.getElementById("preset-select"));
-        return;
-    }
-
-    document.getElementById("rnbo-presets").removeChild(document.getElementById("no-presets-label"));
-    let presetSelect = document.getElementById("preset-select");
-    presets.forEach((preset, index) => {
-        const option = document.createElement("option");
-        option.innerText = preset.name;
-        option.value = index;
-        presetSelect.appendChild(option);
-    });
-    presetSelect.onchange = () => device.setPreset(presets[presetSelect.value].preset);
-}
-
-function makeMIDIKeyboard(device) {
-    let mdiv = document.getElementById("rnbo-clickable-keyboard");
-    if (device.numMIDIInputPorts === 0) return;
-
-    mdiv.removeChild(document.getElementById("no-midi-label"));
-
-    const midiNotes = [49, 52, 56, 63];
-    midiNotes.forEach(note => {
-        const key = document.createElement("div");
-        const label = document.createElement("p");
-        label.textContent = note;
-        key.appendChild(label);
-        key.addEventListener("pointerdown", () => {
-            let midiChannel = 0;
-
-            // Format a MIDI message paylaod, this constructs a MIDI on event
-            let noteOnMessage = [
-                144 + midiChannel, // Code for a note on: 10010000 & midi channel (0-15)
-                note, // MIDI Note
-                100 // MIDI Velocity
-            ];
-        
-            let noteOffMessage = [
-                128 + midiChannel, // Code for a note off: 10000000 & midi channel (0-15)
-                note, // MIDI Note
-                0 // MIDI Velocity
-            ];
-        
-            // Including rnbo.min.js (or the unminified rnbo.js) will add the RNBO object
-            // to the global namespace. This includes the TimeNow constant as well as
-            // the MIDIEvent constructor.
-            let midiPort = 0;
-            let noteDurationMs = 250;
-        
-            // When scheduling an event to occur in the future, use the current audio context time
-            // multiplied by 1000 (converting seconds to milliseconds) for now.
-            let noteOnEvent = new RNBO.MIDIEvent(device.context.currentTime * 1000, midiPort, noteOnMessage);
-            let noteOffEvent = new RNBO.MIDIEvent(device.context.currentTime * 1000 + noteDurationMs, midiPort, noteOffMessage);
-        
-            device.scheduleEvent(noteOnEvent);
-            device.scheduleEvent(noteOffEvent);
-
-            key.classList.add("clicked");
-        });
-
-        key.addEventListener("pointerup", () => key.classList.remove("clicked"));
-
-        mdiv.appendChild(key);
-    });
-}
-
-setup();
-
-
-
+// this gets called every render frame
+// (which is usually 60 times per second
 function draw() {
-     background(mouseY / 2, 100, 100);
-
-     fill(360 - mouseY/ 2, 100, 100);
-
-     rect (360, 360, mouseX + 1, mouseX +1);
-
-    let yValue = map(mouseY, height, 0, 0, 1);
-
-    let xValue = map(mouseX, 0, width, 0.01, 100);
-
-    yValue = yValue / 1;
-
-    xValue = xValue / 100;
-
-
-
-
-    if(buzzParam) {
-        buzzParam.normalizedValue = yValue;
-
-    }
-
-    if (mixParam){
-        mixParam.normalizedValue = xValue;
-    }
-
+  // this will re-draw the canvas each frame (60 times per second)
+  // with a very low opacity
+  background('rgba(255, 255, 0, 0.05)')
 }
-
