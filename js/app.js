49a8de2e266f879b87be3793fc1bffbcae74a3ee
Yashique Chalil <yashiquechalil@gmail.com>
2026-04-19 22:47:00 +0100

    stle changes + particle js shinans

--- js/app.js
+++ js/app.js
@@ -1,12 +1,9 @@
-// Semi-circular wave animation aligned to the left integrated with RNBO setup
-let lines = []; // Array to hold multiple lines
-let numLines = 100; // Number of semi-circular waves
-let waveHeight = 20; // Amplitude of the waves
-let waveLength = 2000; // Wavelength of the waves
-let speed = 0.01; // Speed of the wave animation
-let lineSpacing = 20; // Space between the lines
+// WebGL/Mesh-like particle system integrated with RNBO setup
+let particles = [];
+const numParticles = 120;
+const connectDistance = 120;
 
-let canvas, w, h, sketchStarted = false, context, fft, startParam, buzzParam, mixParam;
+let canvas, w, h, sketchStarted = false, context, fft, startParam, buzzParam, mixParam, doomFreq;
 let isPlaying = false;
 let waveform = [];
 let startButton, playIcon, pauseIcon;
@@ -17,7 +14,7 @@ async function rnboSetup(context) {
     const outputNode = context.createGain();
     outputNode.connect(context.destination);
 
-    response = await fetch(patchExportURL);
+    const response = await fetch(patchExportURL);
     const doomPatcher = await response.json();
 
     const doomDevice = await RNBO.createDevice({ context, patcher: doomPatcher });
@@ -26,7 +23,6 @@ async function rnboSetup(context) {
     mixParam = doomDevice.parametersById.get('doomFuzz/Mix');
     buzzParam = doomDevice.parametersById.get('doomFuzz/DoomFuzzDSP/Fuzz/Buzz');
     doomFreq = doomDevice.parametersById.get('doomFuzz/DoomFuzzDSP/Doom/DoomFreqShift');
-    //doomFreq = doomDevice.parametersById.get('doomFuzz/DoomFuzzDSP/Doom/FreqShift');
 
     fft.setInput(outputNode);
 
@@ -36,15 +32,17 @@ async function rnboSetup(context) {
 
 function setup() {
     w = windowWidth;
-    h = 800;
+    h = windowHeight; // Full window height for the particle network
     canvas = createCanvas(w, h);
     canvas.parent('patcherCanvas');
-    noFill();
-    strokeWeight(50); // Lines are now 10px in width
     angleMode(RADIANS);
 
     // Create circular start/stop button
+    let hero = select('#hero');
     startButton = createButton('');
+    if (hero) {
+        startButton.parent(hero);
+    }
     startButton.addClass('start-button');
     startButton.mousePressed(resumeAudio);
     
@@ -54,18 +52,18 @@ function setup() {
     pauseIcon.hide();
     startButton.child(playIcon);
     startButton.child(pauseIcon);
-    playIcon.addClass('icon play-icon'); // Applies styles for the play icon
-    pauseIcon.addClass('icon pause-icon'); // Applies styles for the pause icon
+    playIcon.addClass('icon play-icon'); 
+    pauseIcon.addClass('icon pause-icon'); 
 
-
-
+    // Initialize audio
     context = getAudioContext();
     fft = new p5.FFT(0.8, 2048);
     rnboSetup(context);
     readOrient();
 
-    for (let i = 0; i < numLines; i++) {
-        lines.push(new SemiCircularWave(i));
+    // Initialize particles
+    for (let i = 0; i < numParticles; i++) {
+        particles.push(new Particle(random(width), random(height)));
     }
 }
 
@@ -76,12 +74,12 @@ function resumeAudio() {
     }
 
     if (isPlaying == false) {
-        startParam.enumValue = 'start';
+        if(startParam) startParam.enumValue = 'start';
         isPlaying = true;
         playIcon.hide();
         pauseIcon.show();
     } else {
-        startParam.enumValue = 'stop';
+        if(startParam) startParam.enumValue = 'stop';
         isPlaying = false;
         pauseIcon.hide();
         playIcon.show();
@@ -89,52 +87,117 @@ function resumeAudio() {
 }
 
 function draw() {
-    background(211, 211, 211); // Light background
-    translate(-height / 2, height / 2); // Align animation to the left
+    clear(); // Clear canvas completely so trail effects don't break scrolling or overlap text endlessly
 
     // Map mouse inputs to audio parameters
     let yValue = map(mouseY, height, 0, 0, 1) / 1;
     let xValue = map(mouseX, 0, width, 0.01, 100) / 100;
 
     if (buzzParam) {
-        buzzParam.normalizedValue = yValue;
+        buzzParam.normalizedValue = constrain(yValue, 0, 1);
     }
-
     if (mixParam) {
-        mixParam.normalizedValue = xValue;
+        mixParam.normalizedValue = constrain(xValue, 0, 1);
     }
 
     // Update FFT waveform
     waveform = fft.waveform();
+    
+    // Calculate global audio intensity (RMS approximated from waveform array)
+    let audioIntensity = 0;
+    if (waveform.length > 0) {
+        let sum = 0;
+        for (let i = 0; i < waveform.length; i++) {
+            sum += waveform[i] * waveform[i];
+        }
+        audioIntensity = sqrt(sum / waveform.length);
+    }
 
-    // Draw semi-circular waves
-    for (let line of lines) {
-        line.update(waveform);
-        line.display();
+    // Update and draw particles
+    for (let i = 0; i < particles.length; i++) {
+        let p = particles[i];
+        p.update(audioIntensity);
+        p.display();
+        
+        // Draw connections
+        for (let j = i + 1; j < particles.length; j++) {
+            let other = particles[j];
+            let d = dist(p.pos.x, p.pos.y, other.pos.x, other.pos.y);
+            
+            // Interaction distance expands based on audio intensity
+            let dynamicConnectDistance = connectDistance + (audioIntensity * 200);
+            
+            if (d < dynamicConnectDistance) {
+                let alpha = map(d, 0, dynamicConnectDistance, 150, 0);
+                stroke(20, 20, 20, alpha);
+                strokeWeight(map(d, 0, dynamicConnectDistance, 1.5, 0.1) + (audioIntensity * 3));
+                
+                // If distortion is high (cursor is high up on the screen yValue approaches 1)
+                // Draw a jagged, agitated line instead of a straight one
+                if (yValue > 0.1 && audioIntensity > 0.01) {
+                    let segments = floor(map(yValue, 0, 1, 2, 8)); // More segments = more jagged
+                    let agitationMagnitude = map(yValue, 0, 1, 0, 15) * audioIntensity * 10;
+                    
+                    noFill();
+                    beginShape();
+                    vertex(p.pos.x, p.pos.y);
+                    for(let s = 1; s < segments; s++) {
+                        let t = s / segments;
+                        // Interpolate point
+                        let ix = lerp(p.pos.x, other.pos.x, t);
+                        let iy = lerp(p.pos.y, other.pos.y, t);
+                        // Add perpendicular agitation
+                        let offsetX = random(-agitationMagnitude, agitationMagnitude);
+                        let offsetY = random(-agitationMagnitude, agitationMagnitude);
+                        vertex(ix + offsetX, iy + offsetY);
+                    }
+                    vertex(other.pos.x, other.pos.y);
+                    endShape();
+                } else {
+                    // Draw normal straight line
+                    line(p.pos.x, p.pos.y, other.pos.x, other.pos.y);
+                }
+            }
+        }
     }
 }
 
-class SemiCircularWave {
-    constructor(index) {
-        this.index = index;
-        this.radius = 100 + index * (50 + lineSpacing);
+class Particle {
+    constructor(x, y) {
+        this.pos = createVector(x, y);
+        this.vel = p5.Vector.random2D().mult(random(0.2, 1.2));
+        this.acc = createVector(0, 0);
+        this.baseSize = random(1, 4);
+        this.mass = this.baseSize;
     }
 
-    update(waveform) {
-        this.waveform = waveform;
-        this.time = frameCount * speed;
+    update(intensity) {
+        // Audio reactivity increases velocity
+        this.vel.add(this.acc);
+        this.vel.limit(2 + (intensity * 10)); // Max speed goes up when loud
+        this.pos.add(this.vel);
+        this.acc.mult(0); // Reset acceleration
+        
+        // Mouse repulsion 
+        let mouseNode = createVector(mouseX, mouseY);
+        let d = dist(this.pos.x, this.pos.y, mouseNode.x, mouseNode.y);
+        if (d < 150) {
+            let force = p5.Vector.sub(this.pos, mouseNode);
+            force.setMag(map(d, 0, 150, 1.5, 0)); // Stronger repulsion closer to mouse
+            this.vel.add(force);
+        }
+
+        // Wrap around screen
+        if (this.pos.x < 0) this.pos.x = width;
+        if (this.pos.x > width) this.pos.x = 0;
+        if (this.pos.y < 0) this.pos.y = height;
+        if (this.pos.y > height) this.pos.y = 0;
     }
 
     display() {
-        stroke(lerpColor(color(0, 128, 128), color(100, 200, 255), this.index / numLines));
-        beginShape();
-        for (let angle = -HALF_PI; angle <= HALF_PI; angle += 0.1) {
-            let audioModulation = this.waveform ? this.waveform[Math.floor(map(angle, -HALF_PI, HALF_PI, 0, this.waveform.length))] * waveHeight : 0;
-            let x = cos(angle) * (this.radius + (audioModulation * 2));
-            let y = sin(angle) * (this.radius + (audioModulation * 10));
-            curveVertex(x, y);
-        }
-        endShape();
+        noStroke();
+        fill(20, 20, 20, 200);
+        ellipse(this.pos.x, this.pos.y, this.baseSize * 1.5);
     }
 }
 
@@ -143,35 +206,23 @@ function windowResized() {
 }
 
 function readOrient() {
-if (window.DeviceOrientationEvent) {
-    window.addEventListener(
-        "deviceorientation",
-        (event) => {
-            const rotateDegrees = event.alpha; // alpha: rotation around z-axis
-            const leftToRight = event.gamma; // gamma: left to right
-            const frontToBack = event.beta; // beta: front back motion
-
-            handleOrientationEvent(frontToBack, leftToRight, rotateDegrees);
-        },
-        true,
-    );
-}
-else
-{
-    throw new Error("no sensors here .-.");
+    if (window.DeviceOrientationEvent && typeof window.addEventListener === 'function') {
+        window.addEventListener(
+            "deviceorientation",
+            (event) => {
+                const rotateDegrees = event.alpha; 
+                const leftToRight = event.gamma; 
+                const frontToBack = event.beta; 
+                handleOrientationEvent(frontToBack, leftToRight, rotateDegrees);
+            },
+            true
+        );
+    }
 }
 
 const handleOrientationEvent = (frontToBack, leftToRight, rotateDegrees) => {
-    // if(buzzParam) {
-    //     doomFreq.normalizedValue = frontToBack;
-
-    // }
-
-    if (mixParam){
-        doomFreq.normalizedValue = leftToRight;
+    if (doomFreq) {
+        doomFreq.normalizedValue = constrain(map(leftToRight, -90, 90, 0, 1), 0, 1);
     }
 };
-
-
-}
 
